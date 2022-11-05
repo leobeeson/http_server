aa5f5fa147c5f44d4253531e621b193d441e57d5
Francis Beeson <leobeeson@gmail.com>
2022-11-05 20:04:07 +0000

    Split server components into modules.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,54 @@
+use server::Server;
+use http::request::Request;
+use http::method::Method;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
+mod server {
+    pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self {
+                addr
+            }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+        }
+    }
 }
 
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
+mod http {
+
+    pub mod request {
+        
+        use super::method::Method;
+        
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
         }
     }
 
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+    pub mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
     }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }
