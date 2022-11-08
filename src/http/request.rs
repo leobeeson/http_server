7662d9944899096b84281f51694315669f3bc22a
Francis Beeson <leobeeson@gmail.com>
2022-11-08 23:07:28 +0000

    Add request string parser function to try_from.

--- src/http/request.rs
+++ src/http/request.rs
@@ -14,9 +14,15 @@ impl TryFrom <&[u8]> for Request {
     type Error = ParseError;
  
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        let request = str::from_utf8(buf)?;
+        let request = str::from_utf8(buf)?;        
         
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
 
         unimplemented!()
     }
@@ -24,7 +30,7 @@ impl TryFrom <&[u8]> for Request {
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
     for (i, c) in request.chars().enumerate() {
-        if c == ' ' {
+        if c == ' ' || c == '\r' {
             return Some((&request[..i], &request[i + 1..]));
         }
     }
