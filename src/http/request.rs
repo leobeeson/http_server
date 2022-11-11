c84a5709311529d9af06783f8d4ceba9e165bd03
Francis Beeson <leobeeson@gmail.com>
2022-11-11 19:10:51 +0000

    Replace query parameters in request with QueryString struct.

--- src/http/request.rs
+++ src/http/request.rs
@@ -3,10 +3,11 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str::{self, Utf8Error};
+use super::QueryString;
 
 pub struct Request<'buf> {
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
     method: Method,
 }
 
@@ -28,7 +29,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
 
         let mut query_string = None;
         if let Some(i) = path.find('?') {
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(QueryString::from(&path[i + 1..]));
             path = &path[..i]
         }
 
