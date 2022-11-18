8ca34e887e692d091e657e980446397afa5710c8
Francis Beeson <leobeeson@gmail.com>
2022-11-18 12:43:45 +0000

    Create Response struct.

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,6 +2,7 @@ pub use method::Method;
 pub use request::ParseError;
 pub use request::Request;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
 
 pub mod method;
 pub mod request;
