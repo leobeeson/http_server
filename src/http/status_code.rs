50ac8a092b88c9356a685337c3b05133189a8c89
Francis Beeson <leobeeson@gmail.com>
2022-11-18 12:51:12 +0000

    Implement StatusCode enum and response messages.

---
+++ src/http/status_code.rs
@@ -0,0 +1,16 @@
+
+pub enum StatusCode {
+    Ok = 200,
+    BadRequest = 400,
+    NotFound = 404,
+}
+
+impl StatusCode {
+    pub fn reason_phrase(&self) -> &str {
+        match self {
+            Self::Ok => "Ok",
+            Self::BadRequest => "Bad Request",
+            Self::NotFound => "Not Found",
+        }
+    }
+}
