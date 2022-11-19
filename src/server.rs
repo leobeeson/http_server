a9fee7f0c291be641cfeed4fc50fd6758ecc6b82
Francis Beeson <leobeeson@gmail.com>
2022-11-19 23:21:00 +0000

    Refactor handling of Response object.

--- src/server.rs
+++ src/server.rs
@@ -24,16 +24,18 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
-                                    let response = Response::new(StatusCode::NotFound, Some("<h1> IT WORKS!!! </h1>".to_string()));
-                                    response.send(&mut stream);
+                                    Response::new(StatusCode::NotFound, Some("<h1> IT WORKS!!! </h1>".to_string()))
                                 }
                                 Err(e) => {
                                   println!("Failed to parse a request: {}", e);
-                                  Response::new(StatusCode::BadRequest, None).send(&mut stream);
+                                  Response::new(StatusCode::BadRequest, None)
                                 }
+                            };
+                            if let Err(e) = response.send(&mut stream) {
+                              println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
