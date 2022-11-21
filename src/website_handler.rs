2889cf2c14d4f2dde2be10659a254427c351a9ef
Francis Beeson <leobeeson@gmail.com>
2022-11-21 21:52:04 +0000

    Add routing to home page.

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,4 +1,4 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Request, Response, StatusCode, Method};
 
 use super::server::Handler;
 
@@ -6,6 +6,13 @@ pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
-    Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+    // Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+    match request.method() {
+      Method::GET => match request.path() {
+        "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+        _ => Response::new(StatusCode::NotFound, None),
+      }
+      _ => Response::new(StatusCode::NotFound, None),
+    }
   }
 }
