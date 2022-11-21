80c6b62fec95e327df815224e450d4b4a1827746
Francis Beeson <leobeeson@gmail.com>
2022-11-21 22:03:13 +0000

    Add path to public resources for serving html files.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use http::Method;
 use http::Request;
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
+    let public_path = env::var("PUBLIC_PATH").unwrap();
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -2,7 +2,15 @@ use crate::http::{Request, Response, StatusCode, Method};
 
 use super::server::Handler;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+  public_path: String
+}
+
+impl WebsiteHandler {
+  pub fn new(public_path: String) -> Self {
+    Self {public_path}
+  }
+}
 
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
@@ -10,6 +18,7 @@ impl Handler for WebsiteHandler {
     match request.method() {
       Method::GET => match request.path() {
         "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+        "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
         _ => Response::new(StatusCode::NotFound, None),
       }
       _ => Response::new(StatusCode::NotFound, None),
