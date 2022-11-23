c2df2579b0dab7261a8c397a4566af756ad512ac
Francis Beeson <leobeeson@gmail.com>
2022-11-23 22:35:57 +0000

    Serve style.css to the / endpoint.

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,6 +1,6 @@
 use crate::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
-use std::fs;
+use std::{fs, path};
 
 pub struct WebsiteHandler {
   public_path: String
@@ -19,11 +19,14 @@ impl WebsiteHandler {
 
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
-    // Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
     match request.method() {
       Method::GET => match request.path() {
         "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
         "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+        path => match self.read_file(path) {
+          Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+          None => Response::new(StatusCode::NotFound, None),
+        }
         _ => Response::new(StatusCode::NotFound, None),
       }
       _ => Response::new(StatusCode::NotFound, None),
